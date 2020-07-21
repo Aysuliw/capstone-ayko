e0c1c4c0224c35f559c6fc0b9bf4782b8f556a5c
Aysuliw <aysulu.mambetnazarova@gmail.com>
2020-07-21 19:17:35 +0500

    final commit

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-export DATABASE_URL='postgresql://postgres:ayko1999@localhost:5432/capstone'
+export DATABASE_URL='postgres://jkqgwuuabadwwf:05c20f2c1540f266955e335cf75eccb347bdb78bc7976153ca06712e96fc596d@ec2-54-236-146-234.compute-1.amazonaws.com:5432/dcf40dlselq406'
 export AUTH0_DOMAIN = 'fsnd-ma.auth0.com'
 export ALGORITHMS = ['RS256']
 export API_AUDIENCE = 'casting-agency'
